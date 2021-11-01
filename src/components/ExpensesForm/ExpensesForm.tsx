a49f225bb02d04deb014205b1a3ed6caaed1a9b1
leonardoarruda-nutrien <leonardo.mesquita@nutrien.com>
2021-11-01 01:12:21 -0300

    feat: adding expense form

--- src/components/ExpensesForm/ExpensesForm.tsx
+++ src/components/ExpensesForm/ExpensesForm.tsx
@@ -1,13 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
+
 import { HeaderText } from "../HeaderText";
 import { ExpensesListItem } from "../ExpensesListItem";
+import { Button } from "../Button";
+import { ADD_EXPENSE } from "../../redux/actions/expensesActions";
+import { selectExpenses } from "../../redux/selectors/expensesSelectors";
 
 import "./styles.css";
 
 export function ExpensesForm() {
+  const dispatch = useDispatch();
+  const expenses = useSelector(selectExpenses);
+  const handleAddExpense = () => dispatch({ type: ADD_EXPENSE });
+
   return (
     <div className="expenses-form">
       <HeaderText>Expenses List</HeaderText>
-      <ExpensesListItem />
+      {expenses.map((item, index) => (
+        <ExpensesListItem key={index} index={index} />
+      ))}
+      <Button title="+ Add Expense" onClick={handleAddExpense} />
     </div>
   );
 }
