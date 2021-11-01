b9d7eeccaf9991ec15e8cfb1aaeb1aaffb3a72d7
leonardoarruda-nutrien <leonardo.mesquita@nutrien.com>
2021-11-01 01:06:06 -0300

    feat: adding awesome icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import Icons from "./icons";
+
 import "./global.css";
 
 import { ExpensesPage } from "../src/pages/ExpensesPage";
+
+library.add(Icons);
 
 function App() {
   return <ExpensesPage />;
---
+++ src/icons.tsx
@@ -0,0 +1,9 @@
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+const icons = {
+  faTrash,
+  faPlus,
+};
+
+export default icons;
