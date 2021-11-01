d2477e34d53adbbf36f12fd83aeddf1df4e981b9
leonardoarruda-nutrien <leonardo.mesquita@nutrien.com>
2021-11-01 01:12:54 -0300

    feat: adding expense list item

--- src/components/ExpensesListItem/ExpensesListItem.tsx
+++ src/components/ExpensesListItem/ExpensesListItem.tsx
@@ -1,12 +1,37 @@
 import { TextInput } from "../TextInput";
+import { IconButton } from "../IconButton";
 
 import "./styles.css";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  DELETE_EXPENSE,
+  UPDATE_EXPENSE,
+} from "../../redux/actions/expensesActions";
+import { selectExpenses } from "../../redux/selectors/expensesSelectors";
 
-export function ExpensesListItem() {
+type Props = {
+  index: number;
+};
+
+export function ExpensesListItem({ index }: Props) {
+  const expenses = useSelector(selectExpenses);
+  const dispatch = useDispatch();
+
+  const handleDeleteExpense = (index: number) =>
+    expenses.length > 1 &&
+    dispatch({ type: DELETE_EXPENSE, payload: { index } });
+
+  const handleUpdateExpense = (index: number) =>
+    dispatch({ type: UPDATE_EXPENSE, payload: { index } });
+
   return (
     <div className="expense-list-item">
-      <TextInput />
-      <label className="delete-button">Delete</label>
+      <TextInput type="text" onChange={() => handleUpdateExpense(index)} />
+      <IconButton
+        iconProps={{ icon: "trash", color: "#D1D1D1", size: "lg" }}
+        buttonProps={{ onClick: () => handleDeleteExpense(index) }}
+      />
+      <TextInput type="number" />
     </div>
   );
 }
