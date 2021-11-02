2ffade3b4b57a6558f258a2c3d86e65664b745e3
leonardoarruda-nutrien <leonardo.mesquita@nutrien.com>
2021-11-02 19:26:53 -0300

    fix: moving get store logic to selectors

--- src/components/ExpensesTotal/ExpensesTotal.tsx
+++ src/components/ExpensesTotal/ExpensesTotal.tsx
@@ -1,27 +1,25 @@
 import { useSelector } from "react-redux";
-import { Expense } from "../../redux";
-import { selectExpenses } from "../../redux/selectors/expensesSelectors";
+import {
+  selectExpensesTotal,
+  selectFilledExpenses,
+} from "../../redux/selectors/expensesSelectors";
 import { Divider } from "../Divider";
 import { ExpenseItem } from "../ExpenseItem";
 
 import "./styles.css";
 
 export function ExpensesTotal() {
-  const expenses = useSelector(selectExpenses);
-  const filteredExpenses = expenses.filter((item) => item.name && item.price);
-  const totalPrice = filteredExpenses.reduce(
-    (prev: number, curr: Expense) => Number(prev) + Number(curr.price),
-    0
-  );
+  const totalPrice = useSelector(selectExpensesTotal);
+  const filteredExpenses = useSelector(selectFilledExpenses);
 
   return (
     <div className="expenses-total">
       <h2 className="expenses-total__title">Expenses</h2>
 
-      {!totalPrice ? (
+      {filteredExpenses.length ? (
         <>
-          {filteredExpenses.map(({ name, price }) => (
-            <ExpenseItem name={name} price={price} />
+          {filteredExpenses.map(({ name, price }, index) => (
+            <ExpenseItem name={name} price={price} key={index} />
           ))}
           <Divider direction="row" />
           <ExpenseItem name="Total" price={totalPrice} />
--- src/redux/selectors/expensesSelectors/expensesSelectors.ts
+++ src/redux/selectors/expensesSelectors/expensesSelectors.ts
@@ -1,3 +1,12 @@
-import { RootStore } from "../../index";
+import { Expense, RootStore } from "../../index";
 
-export const selectExpenses = (state: RootStore) => state.expenses;
+export const selectExpenses = (store: RootStore) => store.expenses;
+
+export const selectExpensesTotal = (store: RootStore) =>
+  store.expenses.reduce(
+    (prev: number, curr: Expense) => Number(prev) + Number(curr.price),
+    0
+  );
+
+export const selectFilledExpenses = (store: RootStore) =>
+  store.expenses.filter((expense) => expense.name && expense.price);
