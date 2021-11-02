7a53026895797288003a1c979fac471de877d332
leonardoarruda-nutrien <leonardo.mesquita@nutrien.com>
2021-11-02 19:28:03 -0300

    feat: expense list item logic

--- src/components/ExpensesListItem/ExpensesListItem.tsx
+++ src/components/ExpensesListItem/ExpensesListItem.tsx
@@ -8,6 +8,7 @@ import {
   UPDATE_EXPENSE,
 } from "../../redux/actions/expensesActions";
 import { selectExpenses } from "../../redux/selectors/expensesSelectors";
+import { useEffect, useState } from "react";
 
 type Props = {
   index: number;
@@ -16,22 +17,42 @@ type Props = {
 export function ExpensesListItem({ index }: Props) {
   const expenses = useSelector(selectExpenses);
   const dispatch = useDispatch();
+  const [expenseName, setExpenseName] = useState<string>("");
+  const [expensePrice, setExpensePrice] = useState<string>("");
+
+  useEffect(() => {
+    dispatch({
+      type: UPDATE_EXPENSE,
+      payload: { index, expense: { name: expenseName, price: expensePrice } },
+    });
+  }, [dispatch, expenseName, expensePrice, index]);
 
   const handleDeleteExpense = (index: number) =>
     expenses.length > 1 &&
     dispatch({ type: DELETE_EXPENSE, payload: { index } });
 
-  const handleUpdateExpense = (index: number) =>
-    dispatch({ type: UPDATE_EXPENSE, payload: { index } });
+  const handleUpdateExpenseName = (expenseName: string) => {
+    setExpenseName(expenseName);
+  };
+
+  const handleUpdateExpensePrice = (expensePrice: string) => {
+    setExpensePrice(expensePrice);
+  };
 
   return (
     <div className="expense-list-item">
-      <TextInput type="text" onChange={() => handleUpdateExpense(index)} />
+      <TextInput
+        type="text"
+        onChange={(event) => handleUpdateExpenseName(event.target.value)}
+      />
       <IconButton
         iconProps={{ icon: "trash", color: "#D1D1D1", size: "lg" }}
         buttonProps={{ onClick: () => handleDeleteExpense(index) }}
       />
-      <TextInput type="number" />
+      <TextInput
+        type="number"
+        onChange={(event) => handleUpdateExpensePrice(event.target.value)}
+      />
     </div>
   );
 }
